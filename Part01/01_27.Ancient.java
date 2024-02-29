617b874adee4ef54f07bb40984c4b39f25feb942
brunnapontual <brunnapontual@hotmail.com>
2024-02-29 10:07:25 -0300

    Update 01_27.Ancient.java

--- Part01/01_27.Ancient.java
+++ Part01/01_27.Ancient.java
@@ -1,18 +1,15 @@
-//erro
+ 
 import java.util.Scanner;
  
 public class Ancient {
  
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Give a number:");
-    int n = Integer.valueOf(scanner.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a year:");
+        int n = Integer.valueOf(scanner.nextLine());
  
-    if (n > 0) {
-        System.out.println("The number is positive.");
-    } else {
-        System.out.println("The number is not positive.");
-    }
- 
+        if (n < 2015) {
+            System.out.println("Ancient history!");
+        }
     }
 }
