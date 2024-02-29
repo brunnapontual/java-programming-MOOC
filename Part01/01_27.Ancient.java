842c753030d8424544cc0e6f8e8686c24d29089b
brunnapontual <brunnapontual@hotmail.com>
2024-02-29 10:06:25 -0300

    Create 01_27.Ancient.java

---
+++ Part01/01_27.Ancient.java
@@ -0,0 +1,18 @@
+//erro
+import java.util.Scanner;
+ 
+public class Ancient {
+ 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give a number:");
+    int n = Integer.valueOf(scanner.nextLine());
+ 
+    if (n > 0) {
+        System.out.println("The number is positive.");
+    } else {
+        System.out.println("The number is not positive.");
+    }
+ 
+    }
+}
