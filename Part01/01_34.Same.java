29e06a33df1e9fb2b79e3f7b32d55627d4c9f420
brunnapontual <brunnapontual@hotmail.com>
2024-06-03 14:53:48 -0300

    Create 01_34.Same.java

---
+++ Part01/01_34.Same.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class Same {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Write your program here 
+        System.out.println("Enter the first string:");
+        String first = scanner.nextLine();
+        System.out.println("Enter the second string:");
+        String second = scanner.nextLine();
+        
+        if (first.equalsIgnoreCase(second)){
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
+    }
+}
+ 
